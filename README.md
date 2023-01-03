82c90758dcf5989d37350be0bc364c39eb1b2427
leeshahub <leesha1223@naver.com>
2023-01-03 15:13:04 +0900

    fixed untracked files

---
+++ README.md
@@ -0,0 +1,2 @@
+# springboot-Practice
+스프링 부트 + AWS
---
+++ src/test/java/com/leesha/book/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.leesha.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
