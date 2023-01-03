5311a46890b02ba598a19637cac065fa0310aa3c
leeshahub <leesha1223@naver.com>
2023-01-03 17:15:01 +0900

    190p google login ing

---
+++ src/main/java/com/leesha/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,15 @@
+package com.leesha.book.springboot.config.auth.dto;
+
+import com.leesha.book.springboot.domain.user.User;
+import java.io.Serializable;
+public class SessionUser implements Serializable{
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
