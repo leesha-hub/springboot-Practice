e3818505b38e09bdebd09908eb513708fd565473
leeshahub <leesha1223@naver.com>
2023-01-14 21:33:50 +0900

    p340. travis CI S3 연동

--- src/test/java/com/leesha/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/leesha/book/springboot/web/IndexControllerTest.java
@@ -3,7 +3,6 @@ package com.leesha.book.springboot.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -12,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
-//@WebMvcTest(controllers = IndexController.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IndexControllerTest {
 
@@ -21,10 +19,10 @@ public class IndexControllerTest {
 
     @Test
     public void 메인페이지_로딩() {
-        // when
-        String body = this.restTemplate.getForObject("/",String.class);
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
 
-        // then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
