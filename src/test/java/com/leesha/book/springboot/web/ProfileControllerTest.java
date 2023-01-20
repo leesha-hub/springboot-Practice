422d0f9ab9434c06dff7f30be100c2fb72d4a9c7
leeshahub <leesha1223@naver.com>
2023-01-20 13:36:21 +0900

    383p 무중단 배포 스크립트 만들기

--- src/test/java/com/leesha/book/springboot/web/ProfileControllerTest.java
+++ src/test/java/com/leesha/book/springboot/web/ProfileControllerTest.java
@@ -9,12 +9,13 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ProfileControllerTest
-{
+public class ProfileControllerTest {
+
     @LocalServerPort
     private int port;
 
@@ -22,8 +23,9 @@ public class ProfileControllerTest
     private TestRestTemplate restTemplate;
 
     @Test
-    public void  profile은_인증없이_호출된다() throws Exception {
+    public void profile은_인증없이_호출된다() throws Exception {
         String expected = "default";
+
         ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(expected);
