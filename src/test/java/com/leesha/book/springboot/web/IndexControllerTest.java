1c0f9c3dde68b6e6ec3f3843c0b5a83a3249f70a
leeshahub <leesha1223@naver.com>
2023-01-20 21:24:35 +0900

    새해

--- src/test/java/com/leesha/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/leesha/book/springboot/web/IndexControllerTest.java
@@ -23,6 +23,6 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("무중단 배포 사이트 완성완성!!");
+        assertThat(body).contains("아브지 오무이 새해 복 많이 받으셩셩셩 >< 꺄르륵");
     }
 }
